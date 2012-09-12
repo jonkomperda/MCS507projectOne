b57d602b82e906f0f33c42f455931c14fe87d8c2
Jon Komperda <komperda.jon@gmail.com>
2012-09-12 16:28:41 -0500

    Got denoising to work kinda (read notes)
    I got the wave file to come out and play, however rather than filtering
    only it is also speeding up the sound and making you sound like a
    chipmunk.
    
    i'm assuming this is due to the fact that the filter function is
    truncating the array, however my attempt at fixing this with the
    'return 0' in the filter function did not alleviate this.

--- src/denoisingSignal.py
+++ src/denoisingSignal.py
@@ -1,6 +1,6 @@
 from scipy.io import wavfile
 from numpy import *
-#from scitools.sound import write
+from scitools.sound import *
 import matplotlib.pyplot as plt
 
 inFile = './static_noise_test.wav'
@@ -11,10 +11,14 @@ soundArray = w[1]
 
 F = fft.rfft(soundArray)
 
-def filt(x): return x >= 400000 #this is the filter function
+def filt(x): 
+    if abs(x) >= 10000:
+        return x #this is the filter function
+    else:
+        return 0
 
 out = filter(filt,F)
-
+#out = F
 iF = fft.irfft(out)
 print iF
 iF = array( [int(x) for x in iF ])
@@ -23,7 +27,7 @@ print type(iF)
 #plt.plot(out,'r--')
 #plt.show()
 
-wavfile.write('outfile.wav',44100,iF)
+play(iF)
 
 
 #print [i for i in array]
