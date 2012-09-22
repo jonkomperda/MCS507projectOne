348c3012e4cc5e787de7845894581b9c3129ef92
Jon Komperda @Laptop <Komperda.Jon@gmail.com>
2012-09-22 14:48:47 -0500

    updates

--- src/denoisingSignal.py
+++ src/denoisingSignal.py
@@ -5,9 +5,7 @@ def plotIt(data):
 
 def readTone():
     from scipy.io import wavfile
-    
     inFile = './static_noise_test.wav'
-    
     tone = wavfile.read(inFile)
     return tone[1]
     
@@ -21,27 +19,41 @@ def filterFFT(data,ampfiltreal=40000,ampfiltimag=100000,highpass=300,lowpass=100
     temp = p(data)
     
     for i in range(len(temp[0][:])):
+        # amplitude filter in real
         if temp[0][i] <= ampfiltreal:
             data[i] = 0
         else:
             data[i] *= 10
+        # amplitude filter in imaginary
         if temp[1][i] <= ampfiltimag:
             data[i] = 0
+        # highpass filter in real and imaginary
         if i < highpass:
             data[i] = 0
+        # lowpass filter in real and imaginary
         if i > lowpass:
             data[i] = 0
     return data
+
+def writeCSV(data,fileName):
+    import csv
+    import numpy as np
+    writer = csv.writer(open(fileName, 'wb'), delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['Frequency,Amplitude'])
+    for i in range(len(data)):
+        if i>=1000:
+            freq = i-1000
+            writer.writerow([freq,np.real(data[i])])
 
 if __name__ == '__main__':
     from numpy import *
     
     tone = readTone()
-
+    
     F = fft.rfft(tone)
-
+    writeCSV(F,'before.csv')
     F = filterFFT(F)
-
+    writeCSV(F,'after.csv')
     result = fft.irfft(F)
 
     writeTone(result)
